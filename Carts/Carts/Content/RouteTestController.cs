dcb83e6018876eaadc078a52e2e8f69069aeca43
wennyu <wennjob+GitHub@gmail.com>
2017-06-12 15:28:59 +0800

    D4

---
+++ Carts/Carts/Content/RouteTestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Carts.Content
+{
+    public class RouteTestController : Controller
+    {
+        // GET: RouteTest
+        public ActionResult Index()
+        {
+            return Content("這是Index");
+        }
+
+        public ActionResult Index2(string id)
+        {
+            return Content(
+                string.Format("id的值為:{0}", id)
+                );
+        }
+
+        public ActionResult Index3(string id, string page)
+        {
+            return Content(
+                string.Format("id的值為:{0},page的值為:{1}",id,page)
+                );
+        }
+    }
+}
