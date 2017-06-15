3cb510b5d9c6c9ba5ed84f64771ca2aa304920c9
wennyu <wennjob+GitHub@gmail.com>
2017-06-15 17:03:25 +0800

    D8_LinQ to Entity_列出

---
+++ Carts/Carts/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Carts.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            //宣告回傳產品列表result
+            List<Models.Product> result = new List<Models.Product>();
+
+            //使用CartsEntities類別，名稱為db
+            using (Models.CartsEntities1 db = new Models.CartsEntities1())
+            {
+                //使用LinQ語法抓取目前Products資料庫中所有資料
+                result = (from s in db.Products select s).ToList();
+            }
+
+               //將 result 傳送給檢視 
+                return View(result);
+        }
+    }
+}
---
+++ Carts/Carts/Models/Carts.edmx.sql
@@ -0,0 +1,60 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 06/15/2017 15:37:49
+-- Generated from EDMX file: C:\WendyJob\[git]]\MaiDotMVCShopTask\Carts\Carts\Models\Carts.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [Carts];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Products'
+CREATE TABLE [dbo].[Products] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(100)  NOT NULL,
+    [Description] nvarchar(max)  NOT NULL,
+    [CategoryId] int  NOT NULL,
+    [Price] decimal(28,3)  NOT NULL,
+    [PublishDate] datetime  NOT NULL,
+    [Status] bit  NOT NULL,
+    [DefaultImageId] bigint  NULL,
+    [Quantity] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'Products'
+ALTER TABLE [dbo].[Products]
+ADD CONSTRAINT [PK_Products]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
