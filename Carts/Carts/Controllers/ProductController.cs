5685dbfc1f2e9088ab76a36eaf7ac35516f2335b
wennyu <wennjob+GitHub@gmail.com>
2017-06-21 15:13:04 +0800

    D10 - 資料驗證

--- Carts/Carts/Controllers/ProductController.cs
+++ Carts/Carts/Controllers/ProductController.cs
@@ -14,13 +14,16 @@ namespace Carts.Controllers
             //宣告回傳產品列表result
             List<Models.Product> result = new List<Models.Product>();
 
+            //接收轉導的成功訊息(將轉導的成功訊息設定給ViewBag變數)
+            ViewBag.ResultMessage = TempData["ResultMessage"];
+            
             //使用CartsEntities類別，名稱為db
             using (Models.CartsEntities1 db = new Models.CartsEntities1())
             {
                 //使用LinQ語法抓取目前Products資料庫中所有資料
                 result = (from s in db.Products select s).ToList();
             }
-
+            //
                //將 result 傳送給檢視 
                 return View(result);
         }
@@ -35,6 +38,12 @@ namespace Carts.Controllers
         [HttpPost] //指定只有使用POST方法才可進入
         public ActionResult Create(Models.Product postback)
         {
+
+            /*判斷如果Product資料驗證通過，則設定成功訊息至TempData，並且轉導致Index頁面。 */
+
+            //如果資料驗証成功
+            if (this.ModelState.IsValid) 
+
             using (Models.CartsEntities1 db = new Models.CartsEntities1())
             {
                 //將回傳資料 postback加入至products
@@ -42,8 +51,19 @@ namespace Carts.Controllers
 
                 //儲存異動資料
                 db.SaveChanges();
+
+                    //設定成功訊息
+                    TempData["ResultMessage"] = String.Format($"商品[{postback.Name}]成功建立");
+
+                    //轉導product/Index頁面
+                    return RedirectToAction("Index");
             }
-            return View();
+
+            //失敗訊息
+            ViewBag.ResultMessage = "資料有誤，請檢查";
+
+            //停留在Create頁面
+            return View(postback);
         }
     
     }
