5be3e5616a8a1a46763e43f89eb737024afa8bdc
wennyu <wennjob+GitHub@gmail.com>
2017-07-04 15:33:32 +0800

    D20_加入 GetCart、AddToCart 方法

--- Carts/Carts/Controllers/CartController.cs
+++ Carts/Carts/Controllers/CartController.cs
@@ -4,15 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+/// <summary>
+///新增取得當前購物車內容的GetCart()方法 與 新增一筆商品進購物車的AddToCart()方法，內部都是呼叫Cart的操作來完成。
+/// </summary>
 
 namespace Carts.Controllers
 {
     public class CartController : Controller
     {
-        // GET: Cart
-        public ActionResult Index()
+        /// <summary>
+        /// 取得目前購物車頁面
+        /// </summary>
+        /// <returns>回傳購物車頁面</returns>
+        public ActionResult GetCart()
         {
-            return View();
+            return PartialView("_CartPartial");
+        }
+
+        /// <summary>
+        /// 以id將 Product 加入至購物車
+        /// </summary>
+        /// <returns>回傳購物車頁面</returns>
+        public ActionResult AddToCart(int id)
+        {
+            var CurrentCart = Models.Carts.Operation.GetCurrentCart();
+            CurrentCart.AddProduct(id);
+            return PartialView("_CartPartial");
         }
     }
 }
