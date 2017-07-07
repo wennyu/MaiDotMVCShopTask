e2d96663b697df98d9533eb4ed1fcfb96153b1b8
wennyu <wennjob+GitHub@gmail.com>
2017-07-07 13:26:39 +0800

    D24.1_結賬頁面_新增Ship模型類別

---
+++ Carts/Carts/Models/OrderModel/Ship.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Carts.Models.OrderModel
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Ship
+    {
+        /// <summary>
+        /// 收貨人姓名
+        /// </summary>
+        [Required]
+        [Display(Name = "收貨人姓名")]
+        [StringLength(maximumLength: 30, ErrorMessage = "{0} 的長度至少必須為 {2} 個字元。"
+            , MinimumLength = 2)]// 字元長度：2~30
+        public string ReceiverName { get; set; }
+
+        /// <summary>
+        /// 收貨人電話
+        /// </summary>
+        [Required]
+        [Display(Name = "收貨人電話")]
+        [StringLength(maximumLength: 15, ErrorMessage = "{0}的長度至少必須為 {2} 個字元。"
+            , MinimumLength = 8)]// 字元長度：8~15
+        public string ReceiverPhone { get; set; }
+
+        /// <summary>
+        /// Gets or sets the receiver address.收貨人住址
+        /// </summary>
+        [Required]
+        [Display(Name = "收貨人地址")]
+        [StringLength(maximumLength: 256, ErrorMessage = "{0} 的長度至少必須為 {2} 個字元。"
+            ,MinimumLength =8)]// 字元長度：8~256
+        public string ReceiverAddress { get; set; }
+
+    }
+}
