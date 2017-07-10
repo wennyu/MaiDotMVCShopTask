a8ab99d4aee6b67d9ec137e080f25517205239fc
wennyu <wennjob+GitHub@gmail.com>
2017-07-10 17:00:39 +0800

    D24.2_顯示目前使用者的購物車

---
+++ Carts/Carts/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+/*Day24.2 新增OrderController，包含Index()方法，
+   此方法主要是顯示目前使用者的購物車內容，
+   等待輸入好運送資訊後，即可以進入訂單資料庫*/
+
+namespace Carts.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Models.OrderModel.Ship model)
+        {
+            return View();
+        }
+    }
+}
