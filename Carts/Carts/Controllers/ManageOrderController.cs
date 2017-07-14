07bae33db7d7c063973e0d2bb41f9e8109452dbc
wennyu <wennjob+GitHub@gmail.com>
2017-07-14 17:15:04 +0800

    D26.1_修改 ManageOrderController

--- Carts/Carts/Controllers/ManageOrderController.cs
+++ Carts/Carts/Controllers/ManageOrderController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+//Day26
+//Index()是顯示目前網站所有訂單
+//Details()是針對每筆訂單顯示購買商品清單
+
 namespace Carts.Controllers
 {
     public class ManageOrderController : Controller
@@ -11,7 +15,33 @@ namespace Carts.Controllers
         // GET: ManageOrder
         public ActionResult Index()
         {
-            return View();
+            using (Models.CartsEntities1 db = new Models.CartsEntities1())
+            {
+                //取得Order中所有資料
+                var result = db.Orders.Select(s => s).ToList();
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (Models.CartsEntities1 db = new Models.CartsEntities1())
+            {
+                //取得 OrderId 為傳入 id 的所有商品列表
+                var result = db.OrderDetails
+                    .Where(w => w.OrderId == id)
+                    .Select(s => s).ToList();
+
+                if (result.Any())
+                {
+                    return View(result);
+                }
+                else
+                {
+                    //如果商品數目為零，代表該訂單異常 (無商品)。導回商品列表。
+                    return RedirectToAction("Index");
+                }
+            }
         }
     }
 }
