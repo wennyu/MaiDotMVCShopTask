d596efd37ed48d3de9dfc611091b51c3fa21822f
wennyu <wennjob+GitHub@gmail.com>
2017-07-14 16:58:05 +0800

    D26_後台訂單列表_新增ManageOrderController

---
+++ Carts/Carts/Controllers/ManageOrderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Carts.Controllers
+{
+    public class ManageOrderController : Controller
+    {
+        // GET: ManageOrder
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
